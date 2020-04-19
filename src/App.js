3d57abc8d684f2e1c357abcf2fd61611c4071138
Ben Lerchin <ben@lerchin.com>
2020-04-19 15:53:41 -0700

    add workspace and artist names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import config from './config';
 
 function App() {
+  console.log(config);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          { config.artists.map(({ name }) =>
+            <li key={name}>{name}</li>
+          )}
+        </ul>
       </header>
     </div>
   );
---
+++ src/config.js
@@ -0,0 +1,28 @@
+export default {
+    artists: [
+        {
+            name: 'Zeynep Abes'
+        }, {
+            name: 'Graham Akins'
+        }, {
+            name: 'Berfin Ataman'
+        }, {
+            name: 'Erin Cooney'
+        }, {
+            name: 'Clara Leivas'
+        }, {
+            name: 'Ben Lerchin'
+        }, {
+            name: "Blaine O'Neill"
+        }, {
+            name: 'Miles Peyton'
+        }, {
+            name: 'Hirad Sab'
+        }, {
+            name: 'Dalena Tran'
+        }, {
+            name: 'Leming Z/C'
+        }
+        
+    ]
+}
