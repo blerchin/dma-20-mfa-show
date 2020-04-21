431d607fdb84cf216088a163221e14c8f583ce82
Ben Lerchin <ben@lerchin.com>
2020-04-20 17:19:00 -0700

    Updates

---
+++ precache-manifest.38b3e5b34552e499a0b07723230fa2b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1446d62fb341c068af170989590d57",
+    "url": "/dma-20-mfa-show/index.html"
+  },
+  {
+    "revision": "f16a8fbc760f8c17e1d2",
+    "url": "/dma-20-mfa-show/static/css/main.5fd46430.chunk.css"
+  },
+  {
+    "revision": "acdaa0da48cb03ebe113",
+    "url": "/dma-20-mfa-show/static/js/2.68527626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dma-20-mfa-show/static/js/2.68527626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16a8fbc760f8c17e1d2",
+    "url": "/dma-20-mfa-show/static/js/main.9625c09d.chunk.js"
+  },
+  {
+    "revision": "814822c3f7b6ed9a3f74",
+    "url": "/dma-20-mfa-show/static/js/runtime-main.452545c6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/dma-20-mfa-show/precache-manifest.38b3e5b34552e499a0b07723230fa2b8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dma-20-mfa-show/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
