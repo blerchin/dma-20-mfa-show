f9f6c06c982e443101eddae2a71adb03638fe36d
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-05-17 22:53:15 -0700

    adding hirad test page and vimeo component

--- src/work/HiradSab.js
+++ src/work/HiradSab.js
@@ -1,7 +1,50 @@
 import React from 'react';
 
+import ProjectHeader from '../Components/ProjectHeader';
+import ArtistBio from '../Components/ArtistBio';
+import ProjectColumns from '../Components/ProjectColumns';
+import Vimeo from '../Components/Video-Vimeo';
+
 export default function() {
     return (
-        <h3>The wonderful work of Hirad Sab</h3>
+        <div className="artist">
+            <ProjectHeader
+                artistName="Hirad Sab"
+                title="Body Troubles"
+                materials="15 Minute Video"
+                link="http://hiradsab.com"
+            />
+            <ProjectColumns
+                columns={[
+                    <Vimeo
+                        url="https://player.vimeo.com/video/397740847?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0"
+                    />
+                   
+                ]}
+            />
+            
+            <ProjectColumns
+                columns={[
+                    <p>
+                        An analysis of the digital proliferation and mediation of the body in the contemporary politics of image,
+                        "Body Troubles" is a rhetorical and discursive attempt at locating the virtual body and its implications
+                        in a climate of economic disparity and political discontent. Its focus is to identify the roles of the
+                        virtual body in discourses, disciplines and socioeconomic milieus, using the state of the art technologies
+                        in machine learning, computer vision, and computer graphics.
+                    </p>
+                ]}
+            />
+
+            <ArtistBio>
+                <p>
+                    Hirad Sab is an Iranian/American artist and visual director whose work explores the margins of digital aesthetics,
+                    internet culture, and technology. His amalgams occupy a precarious intersection of culture and the democratic
+                    nature of image circulation; an aesthetic trend that expands and mutates rapidly. His work has been described as
+                    simultaneously distant and close, drawing the viewer in, even as it challenges them to look away, and heavily
+                    features depictions of the human form, gesture, and activity in distinctly digital environments. The result is in
+                    an emblematic and distinguishable oeuvre that resists easy classification.
+                </p>
+            </ArtistBio>
+        </div>
     );
 }
