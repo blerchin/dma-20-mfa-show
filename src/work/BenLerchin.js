15e035d4431b1521a89cd2118968c7d66e40d2c3
Chandler McWilliams <chandler@brysonian.com>
2020-05-18 23:29:45 -0700

    reformat all works for new components etc

--- src/work/BenLerchin.js
+++ src/work/BenLerchin.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import Artist from '../Containers/Artist';
+import ProjectCover from '../Components/ProjectCover';
 import ProjectHeader from '../Components/ProjectHeader';
 import ArtistBio from '../Components/ArtistBio';
-import ProjectColumns from '../Components/ProjectColumns';
+import ProjectColumns, { Column } from '../Components/ProjectColumns';
 import Image from '../Components/Image';
 import Vimeo from '../Components/VideoVimeo'
 
 import BenImage from './assets/BenLerchin-yimby-cover.jpg';
+import Placeholder from './assets/placeholder.png';
 
 export default function() {
     return (
@@ -18,31 +20,36 @@ export default function() {
                 materials="10 minute video"
                 link="http://benlerchin.com"
             />
-            <ProjectColumns
-                columns={[
+            <ProjectCover>
+                <Image
+                    img={Placeholder}
+                    alt=''
+                />
+            </ProjectCover>
+            <ProjectColumns>
+                <Column>
                     <Vimeo
                         url="https://player.vimeo.com/video/TK?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0"
                     />
-
-                ]}
-            />
-            <ProjectColumns
-                columns={[
+                </Column>
+            </ProjectColumns>
+            <ProjectColumns>
+                <Column>
                     <Image
                         img={BenImage}
                         alt='Still from video. A gray industrial concrete and metal box is in the foreground, with the text
                         "MILE 103" obliquely visible. Low brush growth is seen in the near background, behind which a number
                         of wind turbines are visible.'
                     />
-                ]}
-            />
-            <ProjectColumns
-                columns={[
+                </Column>
+            </ProjectColumns>
+            <ProjectColumns>
+                <Column>
                     <p>
                         Semi-satirical mockumentary reframing Los Angeles' infrastructure as a tourist destination for nature-lovers.
                     </p>
-                ]}
-            />
+                </Column>
+            </ProjectColumns>
             <ArtistBio>
                 <p>
                     Ben Lerchin is an artist and technologist who works with software, photography, and digital fabrication. Their practice deals extensively with the relationship between the photographic image and the American West. Reflecting on the uneasy experience of living between the city and the wilderness, my work attempts to reconcile a networked, industrialized lifestyle with the unstable ground under our feet. It is an attempt to erase boundaries with the natural world, and to see it not as victim nor antagonist, but as co-conspirator and friend. Using digital photographic processes, Ben embeds a polyphony of viewpoints into three dimensional forms reminiscent of the landscape from which they emerge, showing sites of resource management in relation to the people that depend on them.
--- src/work/HiradSab.js
+++ src/work/HiradSab.js
@@ -1,54 +1,59 @@
 import React from 'react';
 
-import Nav from '../Components/Nav';
+import Artist from '../Containers/Artist';
+import ProjectCover from '../Components/ProjectCover';
 import ProjectHeader from '../Components/ProjectHeader';
 import ArtistBio from '../Components/ArtistBio';
-import ProjectColumns from '../Components/ProjectColumns';
+import ProjectColumns, { Column } from '../Components/ProjectColumns';
 import Vimeo from '../Components/VideoVimeo';
+import Image from '../Components/Image';
+
+
+import Placeholder from './assets/placeholder.png';
 
 export default function() {
     return (
-        <div className="artist">
-            <Nav />
+        <Artist>
             <ProjectHeader
                 artistName="Hirad Sab"
                 title="Body Troubles"
                 materials="15 Minute Video"
                 link="http://hiradsab.com"
             />
-            <div className="artistContent">
-                <ProjectColumns
-                    columns={[
-                        <Vimeo
-                            url="https://player.vimeo.com/video/397740847?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0"
-                        />
-
-                    ]}
+            <ProjectCover>
+                <Image
+                    img={Placeholder}
+                    alt=''
                 />
-
-                <ProjectColumns
-                    columns={[
-                        <p>
-                            An analysis of the digital proliferation and mediation of the body in the contemporary politics of image,
-                            "Body Troubles" is a rhetorical and discursive attempt at locating the virtual body and its implications
-                            in a climate of economic disparity and political discontent. Its focus is to identify the roles of the
-                            virtual body in discourses, disciplines and socioeconomic milieus, using the state of the art technologies
-                            in machine learning, computer vision, and computer graphics.
-                        </p>
-                    ]}
-                />
-
-                <ArtistBio>
+            </ProjectCover>
+            <ProjectColumns>
+                <Column>
+                    <Vimeo
+                        url="https://player.vimeo.com/video/397740847?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0"
+                    />
+                </Column>
+            </ProjectColumns>
+            <ProjectColumns>
+                <Column>
                     <p>
-                        Hirad Sab is an Iranian/American artist and visual director whose work explores the margins of digital aesthetics,
-                        internet culture, and technology. His amalgams occupy a precarious intersection of culture and the democratic
-                        nature of image circulation; an aesthetic trend that expands and mutates rapidly. His work has been described as
-                        simultaneously distant and close, drawing the viewer in, even as it challenges them to look away, and heavily
-                        features depictions of the human form, gesture, and activity in distinctly digital environments. The result is in
-                        an emblematic and distinguishable oeuvre that resists easy classification.
+                        An analysis of the digital proliferation and mediation of the body in the contemporary politics of image,
+                        "Body Troubles" is a rhetorical and discursive attempt at locating the virtual body and its implications
+                        in a climate of economic disparity and political discontent. Its focus is to identify the roles of the
+                        virtual body in discourses, disciplines and socioeconomic milieus, using the state of the art technologies
+                        in machine learning, computer vision, and computer graphics.
                     </p>
-                </ArtistBio>
-            </div>
-        </div>
+                </Column>
+            </ProjectColumns>
+            <ArtistBio>
+                <p>
+                    Hirad Sab is an Iranian/American artist and visual director whose work explores the margins of digital aesthetics,
+                    internet culture, and technology. His amalgams occupy a precarious intersection of culture and the democratic
+                    nature of image circulation; an aesthetic trend that expands and mutates rapidly. His work has been described as
+                    simultaneously distant and close, drawing the viewer in, even as it challenges them to look away, and heavily
+                    features depictions of the human form, gesture, and activity in distinctly digital environments. The result is in
+                    an emblematic and distinguishable oeuvre that resists easy classification.
+                </p>
+            </ArtistBio>
+        </Artist>
     );
 }
