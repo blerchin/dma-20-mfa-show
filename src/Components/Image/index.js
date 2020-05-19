682d0036f3adbd929be24563bdde0799884daeda
hsab <hsab@users.noreply.github.com>
2020-05-18 17:42:58 -0700

    react-lazy-load-image-component

--- src/Components/Image/index.js
+++ src/Components/Image/index.js
@@ -1,56 +1,56 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
-import {
-  container,
-  cap,
-  fullHeight,
-} from './style.module.css';
+import { container, cap, fullHeight } from "./style.module.css";
 
 const Image = ({
   img,
   alt,
   caption = false,
   fullHeight: isFullHeight = false,
-  maxHeight = 'auto'
+  maxHeight = "auto",
 }) => {
   const style = {
     maxHeight,
   };
 
+  const parentStyles = {
+    "line-height": "0",
+  };
+
   const [showBG, setShowBG] = useState(true);
 
-  if (maxHeight !== 'auto' && isFullHeight === false) {
-    style.width = 'auto';
+  if (maxHeight !== "auto" && isFullHeight === false) {
+    style.width = "auto";
   }
 
   return (
-      <div className={`${container} ${isFullHeight ? fullHeight : ''}`} style={{
-        backgroundSize: 'cover',
-        backgroundImage: showBG ? 'url("' + img.placeholder + '")' : 'none'
-      }}>
-        <img
-          style={style}
-          src={img.src}
-          srcSet={img.srcSet}
-          onLoad={() => setShowBG(false)}
-          alt={alt}
-        />
-        {
-          caption && (
-            <div className={cap}>
-              {caption}
-            </div>
-          )
-        }
-      </div>
-    );
-}
+    <div className={`${container} ${isFullHeight ? fullHeight : ""}`}>
+      {
+        <div style={{ ...parentStyles }}>
+          <LazyLoadImage
+            src={img.src}
+            srcSet={img.srcSet}
+            alt={alt}
+            placeholderSrc={img.placeholder}
+            threshold={10}
+            effect="blur"
+            useIntersectionObserver={true}
+            style={style}
+          />
+        </div>
+      }
+      {caption && <div className={cap}>{caption}</div>}
+    </div>
+  );
+};
 
 Image.defaultProps = {
-  caption: '',
+  caption: "",
   fillViewport: false,
-  maxHeight: 'auto',
+  maxHeight: "auto",
 };
 
 Image.propTypes = {
