cdacb26b6674e8adbe0e8c668d5bcb5338e3062a
Lauren McCarthy <laurenleemccarthy@gmail.com>
2020-05-26 14:41:26 -0700

    tweak to graham page

--- src/work/GrahamAkins.js
+++ src/work/GrahamAkins.js
@@ -3,6 +3,7 @@ import React from "react";
 import Artist from "../Containers/Artist";
 import Gradient from "../Components/Gradient";
 import ProjectHeader from "../Components/ProjectHeader";
+import ProjectCover from "../Components/ProjectCover";
 import ProjectDescription from "../Components/ProjectDescription";
 import Image from "../Components/Image";
 import ProjectColumns, { Column } from "../Components/ProjectColumns";
@@ -13,7 +14,7 @@ import GrahamImage1 from "./assets/GrahamAkins-1.png";
 
 export default function ({slug, name}) {
   return (
-    <Artist noCover="true">
+    <Artist>
       <Gradient/>
       <Seo
         title={name}
@@ -27,14 +28,12 @@ export default function ({slug, name}) {
         materials="trail cameras, wooden stages, wooden tower, zentai suit, &amp; photogrammetry"
         link="https://grahamakins.cargo.site"
       />
-      <ProjectColumns>
-        <Column>
+      <ProjectCover>
           <Vimeo 
             url="https://player.vimeo.com/video/422619346?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0"
-            padding="125%"
+            isFullHeight="yes"
           />
-        </Column>
-      </ProjectColumns>
+      </ProjectCover>
       <ProjectColumns>
         <Column>
           <Image
