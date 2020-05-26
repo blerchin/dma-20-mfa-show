21cd1ed91b32bb4ad01e7db98d87e4249350df78
Lauren Lee McCarthy <laurenleemccarthy@gmail.com>
2020-05-26 15:25:58 -0700

    Artist pages (#76)
    * adding erin page
    
    * updating zeynep page
    
    * update miles page
    
    * updating berfin page
    
    * updating berfin page
    
    * updating clara page
    
    * updates
    
    * tweaks
    
    * fix homepage spacing, erin image padding
    
    * tweak to graham page
    
    * useLazy:false on Graham page
    
    Co-authored-by: Ben Lerchin <ben@lerchin.com>

---
+++ src/Components/Gradient/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {
+  gradient
+} from './style.module.scss';
+
+const Gradient = () => (
+  <div className={gradient}></div>
+);
+
+export default Gradient;
--- src/work/GrahamAkins.js
+++ src/work/GrahamAkins.js
@@ -1,84 +1,55 @@
 import React from "react";
 
 import Artist from "../Containers/Artist";
-import ProjectCover from "../Components/ProjectCover";
+import Gradient from "../Components/Gradient";
 import ProjectHeader from "../Components/ProjectHeader";
+import ProjectCover from "../Components/ProjectCover";
+import ProjectDescription from "../Components/ProjectDescription";
 import Image from "../Components/Image";
 import ProjectColumns, { Column } from "../Components/ProjectColumns";
 import Vimeo from "../Components/VideoVimeo";
-
-import GrahamImage1 from "./assets/Graham Akins web template image 1.png";
-import GrahamImage2 from "./assets/Graham Akins web template image 2.png";
-import GrahamImage3 from "./assets/Graham Akins web template image 3.png";
-import GrahamImage4 from "./assets/Graham Akins web template image 4.png";
-import GrahamImage5 from "./assets/Graham Akins web template image 5.png";
-import GrahamImage6 from "./assets/Graham Akins web template image 6.png";
-import GrahamImage7 from "./assets/Graham Akins web template image 7.png";
-
-import Placeholder from "./assets/placeholder.png";
-
 import Seo from "../Components/Seo";
+
+import GrahamImage1 from "./assets/GrahamAkins-1.png";
+
 export default function ({slug, name}) {
   return (
     <Artist>
+      <Gradient/>
       <Seo
         title={name}
-        description="Scelerisque venenatis nibh fames ad quam feugiat leo commodo vitae sed lacus."
+        description="A performative investigation of our desire to be close to the animal other in the Anthropocene"
         path={slug}
         image={GrahamImage1}
       />
       <ProjectHeader
         artistName={name}
-        title="Approximate Other"
-        link="https://instagram.com/grahamycakes_"
+        title="Approximate Other (Tower &amp; Stage)"
+        materials="trail cameras, wooden stages, wooden tower, zentai suit, &amp; photogrammetry"
+        link="https://grahamakins.cargo.site"
       />
       <ProjectCover>
-        <Image img={Placeholder} alt="" />
+          <Vimeo 
+            url="https://player.vimeo.com/video/422619346?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0"
+            isFullHeight="yes"
+          />
       </ProjectCover>
-      <ProjectColumns>
-        <Column>
-          <Vimeo url="https://player.vimeo.com/video/?color=ffffff&amp;title=0&amp;byline=0&amp;portrait=0" />
-        </Column>
-      </ProjectColumns>
       <ProjectColumns>
         <Column>
           <Image
             img={GrahamImage1}
-            alt="A grey field with the text 1200x1200"
-          />
-          <Image
-            img={GrahamImage2}
-            alt="A grey field with the text 1200x1200"
-          />
-          <Image
-            img={GrahamImage3}
-            alt="A grey field with the text 1200x1200"
-          />
-          <Image
-            img={GrahamImage4}
-            alt="A grey field with the text 1200x1200"
-          />
-          <Image
-            img={GrahamImage5}
-            alt="A grey field with the text 1200x1200"
-          />
-          <Image
-            img={GrahamImage6}
-            alt="A grey field with the text 1200x1200"
-          />
-          <Image
-            img={GrahamImage7}
+            useLazy={false}
             alt="A grey field with the text 1200x1200"
           />
         </Column>
       </ProjectColumns>
       <ProjectColumns>
         <Column>
-          <h3 class="sr-only">Project Description</h3>
-          <p>
-            Materials: video, photography, photogrammetry, wooden structures,
-            zentai suit, &amp; dry cat food
-          </p>
+          <ProjectDescription>
+            <p>
+              A performative investigation of our desire to be close to the animal other in the Anthropocene
+            </p>
+          </ProjectDescription>
         </Column>
       </ProjectColumns>
     </Artist>
