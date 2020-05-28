9648b8a358221d5a6514967638296871e7e72073
Ben Lerchin <ben@lerchin.com>
2020-05-28 11:44:03 -0700

    Get rid of extra white space

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
