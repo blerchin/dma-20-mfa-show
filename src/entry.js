89a0bb7439663c9fa3acd65321c57f1e6528eb29
Ben Lerchin <ben@lerchin.com>
2020-05-29 11:53:13 -0700

    idk try catching all the promise chains

--- src/entry.js
+++ src/entry.js
@@ -8,4 +8,4 @@ const modernBrowser = (
 const maybePolyfill = modernBrowser ? Promise.resolve() : import('./polyfill.js');
 maybePolyfill.then(() => {
   require('./index.js');
-})
+}).catch((err) => console.log(`sadness ;/ ${err}`))
